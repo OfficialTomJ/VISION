657a8bc8197340b59e2f707c201cf1ee94bdef17
Thomas Johnston <8527295+OfficialTomJ@users.noreply.github.com>
2023-06-02 15:34:12 +1000

    Initial Commit

---
+++ MUSAIC/MUSAICApp.swift
@@ -0,0 +1,17 @@
+//
+//  MUSAICApp.swift
+//  MUSAIC
+//
+//  Created by Thomas Johnston on 2/6/2023.
+//
+
+import SwiftUI
+
+@main
+struct MUSAICApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
