e522d8a1da7dab5c6bdec72bba66bf0f0a1535e0
suleman <sulemandawood911@gmail.com>
2023-06-09 10:18:33 +1000

    Added Sign IN

--- MUSAIC/SignInView.swift
+++ MUSAIC/SignInView.swift
@@ -8,8 +8,53 @@
 import SwiftUI
 
 struct SignInView: View {
+    @State private var text: String = ""
+    @State private var password: String = ""
+    
     var body: some View {
-        Text("Sign someone in here")
+        ZStack(alignment: .top) {
+            Image("Background")
+                .resizable()
+                .frame(width: 450,height: 1000)
+                .ignoresSafeArea(.all)
+            VStack {
+                Text("Welcome to MUSAIC")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .foregroundColor(Color.white)
+                    .padding(.bottom, 1.0)
+                Text("Where all things inspired")
+                    .foregroundColor(Color.white)
+                    .opacity(0.6)
+                    .italic()
+                TextField("Email", text: $text)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .disableAutocorrection(true)
+                    .textInputAutocapitalization(.never)
+                    .padding(.top, 100.0)
+                    .frame(width: 300)
+                    .opacity(0.6)
+                SecureField("Password", text: $password)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .disableAutocorrection(true)
+                    .textInputAutocapitalization(.never)
+                    .keyboardType(.asciiCapable)
+                    .padding(.top, 5.0)
+                    .frame(width: 300)
+                    .opacity(0.6)
+                ZStack (alignment: .center){
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color(red: 1, green: 1, blue: 1))
+                        .frame(width: 200, height: 40)
+                        .opacity(0.3)
+                    Button(action: {}) {
+                        Text("Sign Up")
+                            .font(.title3)
+                            .foregroundColor(Color.white)
+                    }
+                }.padding(.top, 15)
+            }.padding(.top, 250)
+        }
     }
 }
 
